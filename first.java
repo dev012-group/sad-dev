c5ab312d280c8344d4c5d700377d022a202310e5
canberk344 <121580627+canberk344@users.noreply.github.com>
2022-12-29 00:03:34 +0300

    first_commit

---
+++ first.java
@@ -0,0 +1,17 @@
+public class C01_HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+      
+        int sayi=111;
+
+        System.out.println("Hello Java");
+
+        System.out.println(sayi);
+
+
+        int not;
+        not=150;
+        String sonuc;
+             sonuc="final";
+        System.out.println(sonuc + " " + (not));
