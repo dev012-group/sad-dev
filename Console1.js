a74763561cd504bcec734e560e21258614e51210
KHALID SHAIFULLAH <94977283+KHALID-404@users.noreply.github.com>
2023-01-22 11:18:17 +0600

    Update Console1.js

--- Console1.js
+++ Console1.js
@@ -7,3 +7,12 @@ return null;
 main()
 
 // console.log(main)
+console.log("Hello World")
+console.log("Hello World") // 2nd time!
+
+function main() {
+return null;
+}
+main()
+
+// console.log(main)
