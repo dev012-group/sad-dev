888f91e0c41f53e737fb4d5bb9f4e60de796d84a
Divine Adeyeye <87317463+DivineAde@users.noreply.github.com>
2023-03-07 13:50:10 +0100

    Update console.js

--- console.js
+++ console.js
@@ -3,7 +3,7 @@ console.log("Hello World")
 console.log("Hello World") // 2nd time!
 
 function main() {
-return null;
+return null; //it means that the function did not return any valid object or value.
 }
 main()
 
